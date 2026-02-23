6d384dff1ef4d729f42fcf2ab9ed0664911fc54b
1tc-selode <oseletye@gmai.com>
2026-02-23 09:31:06 +0100

    elso

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /*GET /api/books – Listázza az összes könyvet.*/
+
+    public function index()
+    {
+        return response()->json(Book::all(), 200);
+    }
+
+    /*● GET /api/books/{query} – Listázza a query-nek megfelelő íróval vagy címmel
+rendelkező könyveket!*/
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function search($query)
+    {
+        $books = Book::where('title', 'like', "%$query%")->orWhere('author', 'like', "%$query%")->get();
+        return response()->json(Book::all(), 200);
+    }
+}
